3879e98877ae58087552ba715abe6db08bf1227d
Janvi Mahajan <janvimahajan@microsoft.com>
2024-03-13 15:29:05 +0530

    Project Structure Setup

---
+++ Sample Applications/WindowsPluginTemplate/WindowsPluginTemplate/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Windows.Plugins.Endpoints;
+using System.Text.Json.Serialization;
+
+namespace WindowsPluginTemplate
+{
+    [WindowsCopilotPlugin]
+    public sealed class MyPlugin
+    {
+        [Endpoint]
+        public Result SayHello(string personName)
+        {
+            string output = Execution.Current.ResolveString("SayHelloOutput", personName);
+            return new(output) { DescriptionForModel = $"Successfully said hello to {personName}." };
+        }
+
+        // Add more functions to your plugin by using the [Endpoint] attribute:
+        // [Endpoint]
+        // public Result SayGoodbye(string personName)
+        // {
+        //     string output = Execution.Current.ResolveString("SayGoodbyeOutput", personName);
+        //     return new (output) { DescriptionForModel = $"Successfully said goodbye to {personName}." };
+        // }
+    }
+}
